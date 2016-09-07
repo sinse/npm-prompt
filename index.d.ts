c675a2a90bf8602a204c0f3537727500a4d4ab5f
Sinse <bruno.salesses@gmail.com>
2016-09-07 23:24:28 +0200

    remove declare module

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,27 @@
 import {EventEmitter} from 'events';
 
-declare module 'prompt' {
+interface Prompt extends EventEmitter {
+    logger: EventEmitter;
+    started : boolean;
+    paused : boolean;
+    stopped: boolean;
+    allowEmpty: boolean;
+    message: string;
+    delimiter: string;
+    colors: boolean;
+    properties: Object;
+    
+    start(options?: Object): void | Prompt;
+    pause(): void | Prompt;
+    stop(): void | Prompt;
+    resume(): void | Prompt;
+    history(search: number | string): any;
+    get(schema: any[] | Object | string, callback: Function): any;
+    confirm(msg: any[] | Object | string, options?: any, callback?: Function): void;
+    getInput(prop: Object | string, callback: Function): any;
+    addProperties(obj: Object, properties: any[], callback: Function): any;
+}   
 
-    interface Prompt extends EventEmitter {
-        logger: EventEmitter;
-        started : boolean;
-        paused : boolean;
-        stopped: boolean;
-        allowEmpty: boolean;
-        message: string;
-        delimiter: string;
-        colors: boolean;
-        properties: Object;
-        
-        start(options?: Object): void | Prompt;
-        pause(): void | Prompt;
-        stop(): void | Prompt;
-        resume(): void | Prompt;
-        history(search: number | string): any;
-        get(schema: any[] | Object | string, callback: Function): any;
-        confirm(msg: any[] | Object | string, options?: any, callback?: Function): void;
-        getInput(prop: Object | string, callback: Function): any;
-        addProperties(obj: Object, properties: any[], callback: Function): any;
-    }   
-
-    var foo: Prompt;
-    export = foo;
-}
+var foo: Prompt;
+export = foo;
 
